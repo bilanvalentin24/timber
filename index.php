070e1146759e7628daff76a79d44a754f0902fac
bilanvalentin24 <bilanvalentin24@gmail.com>
2022-05-10 16:55:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>timber house</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@300&family=Work+Sans:ital,wght@1,100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Work+Sans:ital,wght@1,100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header" style="background-image: url(../img/background.jpg);">
+       <div class="header__inner">
+           <img src="img/home.png" alt="home">
+           <div class="header__name">
+               timber house
+           </div>
+           <a class="phone" href="tel:+380673001108">+38(067)-300-11-08</a>
+           <div class="header__title">
+               Дім із бруса під ключ
+           </div>
+           <div class="header__sale">
+               <img src="img/sale.png" alt="">
+           </div>
+       </div> 
+    </header>
+    <main class="main">
+        <div class="container">
+        <div class="main__title">
+            Будівництво домів і бань із
+            струганого і профільного бруса
+        </div>
+        <div class="main__text">
+            За 10 років ми звели більше 250 домів і бань із бруса
+            МАЄМО ВЕЛИКИЙ ДОСВІД В БУДІВНИЦТВІ
+            гарантуємо якість і надійність усіх побудованих нами проектів
+
+            ГАРАНТІЯ НА БУДИНКИ І БАНІ ІЗ БРУСА 3 РОКИ
+        </div>
+        <div class="main__img">
+            <img src="img/main-img.png" alt="">
+        </div>
+        <div class="project">
+            <div class="project__title">
+                Проекти будинків із бруса
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    БУДИНОК №1
+                </div>
+                <div class="project__size">
+                    Розмір будинку: 6 х 6
+                </div>
+                <div class="project__area">
+                    Загальна площа будинку: 54 кв. м.
+                </div>
+                <div class="project__price">
+                    Вартість - 40 000 $
+                </div>
+                <div class="project_images">
+                    <div class="project__images-item">
+                        <img src="img/project-images1.jpg" alt="">
+                    </div>
+                    <div class="project__images-item">
+                        <img src="img/project-images2.jpg" alt="">
+                    </div>
+
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    БУДИНОК №1
+                </div>
+                <div class="project__size">
+                    Розмір будинку: 6 х 6
+                </div>
+                <div class="project__area">
+                    Загальна площа будинку: 54 кв. м.
+                </div>
+                <div class="project__price">
+                    Вартість - 40 000 $
+                </div>
+                <div class="project_images">
+                    <div class="project__images-item">
+                        <img src="img/project-images1.jpg" alt="">
+                    </div>
+                    <div class="project__images-item">
+                        <img src="img/project-images2.jpg" alt="">
+                    </div>
+
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    БУДИНОК №1
+                </div>
+                <div class="project__size">
+                    Розмір будинку: 6 х 6
+                </div>
+                <div class="project__area">
+                    Загальна площа будинку: 54 кв. м.
+                </div>
+                <div class="project__price">
+                    Вартість - 40 000 $
+                </div>
+                <div class="project_images">
+                    <div class="project__images-item">
+                        <img src="img/project-images1.jpg" alt="">
+                    </div>
+                    <div class="project__images-item">
+                        <img src="img/project-images2.jpg" alt="">
+                    </div>                
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    БУДИНОК №2
+                </div>
+                <div class="project__size">
+                    Розмір будинку: 6 х 6
+                </div>
+                <div class="project__area">
+                    Загальна площа будинку: 54 кв. м.
+                </div>
+                <div class="project__price">
+                    Вартість - 40 000 $
+                </div>
+                <div class="project_images">
+                    <div class="project__images-item">
+                        <img src="img/project-images1.jpg" alt="">
+                    </div>
+                    <div class="project__images-item">
+                        <img src="img/project-images2.jpg" alt="">
+                    </div>                
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    БУДИНОК №3
+                </div>
+                <div class="project__size">
+                    Розмір будинку: 6 х 6
+                </div>
+                <div class="project__area">
+                    Загальна площа будинку: 54 кв. м.
+                </div>
+                <div class="project__price">
+                    Вартість - 40 000 $
+                </div>
+                <div class="project_images">
+                    <div class="project__images-item">
+                        <img src="img/project-images1.jpg" alt="">
+                    </div>
+                    <div class="project__images-item">
+                        <img src="img/project-images2.jpg" alt="">
+                    </div>                
+                </div>
+            </div>
+        </div>
+    </div>
+            <div class="download">
+               <img src="img/home-download.png" alt="">  
+               <a href="#">ЗАКАЧАТИ ВЕСЬ КАТАЛОГ ДОМІВ</a>              
+            </div>
+            <div class="container">
+            <div class="gallery">
+                <div class="gallery__title">
+                    ФОТОГРАФІЇ НАШИХ РОБІТ
+                </div>
+                <div class="galery__text">
+                    деякі фотографії побудованих нами домів і бань із бруса
+                </div>
+                <div class="galery__inner">
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                    <a href="img/item1.jpg"><img src="img/item1.jpg" alt=""></a>
+                </div>
+            </div>
+        </div>
+        </main>
+        <footer class="footer">
+            <div class="container">
+            <div class="main__img">
+                <img src="img/main-img.png" alt="">
+            </div>
+            <a class="phone" href="tel:+380673001108">+38(067)-300-11-08</a>
+            <div class="footer__mail">
+                mail:bilan_valentin@ukr.net
+            </div>            
+            </div>
+        </footer>
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
